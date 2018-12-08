7f1937c1d4e1fe6ab66db9023724e885005ab542
Matthew Dean <matthew-dean@users.noreply.github.com>
2018-12-08 12:05:13 -0800

    Working parsing

--- test/test.js
+++ test/test.js
@@ -1,4 +1,7 @@
 /**
+// Anything without "from", preserve
+@import {vars} from '';
+@import "something.css";
 
 @fixed something = 'value';
 @fixed colors = {foreground: 'red'};
@@ -20,42 +23,75 @@ const root = (typeof self === 'object' && self.self === self && self) ||
 // 	builtins = {JSON, Object, Function, Array, String, Boolean, Number, Date, RegExp}
 // })()
 
-/** https://stackoverflow.com/questions/2051678/getting-all-variables-in-scope */
-
-const pathsAccessed = []
-
-const store = target => {
-	return new DeepProxy(target, {
-		has(target, prop) { return true },
+const store = obj => {
+	return new DeepProxy(obj, {
 		get(target, path, receiver) {
-			const val = Reflect.get(target, path, receiver) || Reflect.get(root, path, receiver)
+			const val = Reflect.get(target, path, receiver)
+
 			if (typeof val === 'object' && val !== null) {
 				return this.nest(val)
 			} else {
-				if (typeof path === 'string') {
-					pathsAccessed.push(this.path.concat(path))
+				let fullPath = this.path.concat(path)
+				if (val !== undefined) {
+					if (obj._DYNAMIC.indexOf(fullPath[0]) > -1) {
+						obj._DYNAMIC_ACCESSED = true
+					} else {
+						obj._DYNAMIC_ACCESSED = false
+					}
+					return val
 				}
-				return val !== undefined ? val : ''
+				return ''
 			}
 		}
 	})
 }
 
-const vars = {}
-
 const def = config => {
 	const eval = () => {
-		with(store(vars)) {
-			var something = 'value'
-			var colors = {foreground: {a: 'red'}}
-			return `
-				.box {
-					foo: ${something} ${1 + 1};
-				}
-			`
+
+		const hash = Math.random().toString(36).substr(2, 9)
+		const _VARS = {
+			_DYNAMIC: [],
+			_DYNAMIC_ACCESSED: false
 		}
+
+		const template = [
+			'.box {\n  .foo: ',
+			'${something}',
+			' ',
+			'${colors.foreground.a}',
+			' ',
+			'${blah}',
+			';\n}'
+		]
+
+		// vars 0 = fixed, 1 = dynamic
+		_VARS.something = 'value'
+		_VARS.colors = {foreground: {a: 'red'}}
+		_VARS.blah = 'foo'
+		_VARS._DYNAMIC = ['blah', 'colors']
+
+		const _CONTEXT = store(_VARS)
+
+		const CSSVars = []
+
+		const output = template.map((fragment, i) => {
+			const func = new Function(
+				"let val; with(this) { val = `" + fragment + "`}" +
+				"return [val, this._DYNAMIC_ACCESSED]"
+			)
+			const test = func.call(_CONTEXT)
+			if (test[1] === true) {
+				let cssVar = `--var-${hash}-${i}`
+				CSSVars.push(cssVar, function() { return func.call(_CONTEXT) })
+				return `var(${cssVar}, ${test[0]})`
+			}
+			return test[0]
+		})
+		console.log(CSSVars)
+		return output.join('')
 	}
 	return eval()
 }
 
-console.log(def(), pathsAccessed)
+console.log(def())
