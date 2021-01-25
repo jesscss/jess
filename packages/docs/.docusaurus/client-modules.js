03aa74fa2f6c24e56c175ca576fd8dfa8b3e4bcf
Matthew Dean <matthew-dean@users.noreply.github.com>
2021-01-24 20:05:18 -0800

    Changes to docs

--- packages/docs/.docusaurus/client-modules.js
+++ packages/docs/.docusaurus/client-modules.js
@@ -1,8 +1,8 @@
 export default [
-  require("/Users/matthew/git/oss/jess/packages/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/matthew/git/oss/jess/packages/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/matthew/git/oss/jess/packages/docs/node_modules/remark-admonitions/styles/infima.css"),
-  require("/Users/matthew/git/oss/jess/packages/docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/matthew/git/oss/jess/packages/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/matthew/git/oss/jess/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/matthew/git/oss/jess/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/matthew/git/oss/jess/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/matthew/git/oss/jess/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/matthew/git/oss/jess/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/Users/matthew/git/oss/jess/packages/docs/src/css/custom.css"),
 ];
