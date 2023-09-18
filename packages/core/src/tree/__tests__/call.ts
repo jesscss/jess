6075a749e1f717ae504c6ac7ced2f876926773af
Matthew Dean <matthew-dean@users.noreply.github.com>
2023-09-18 07:29:54 -0700

    Lots of awesome changes

--- packages/core/src/tree/__tests__/call.ts
+++ packages/core/src/tree/__tests__/call.ts
@@ -9,13 +9,23 @@ describe('Call', () => {
     context = new Context()
     // out = new OutputCollector()
   })
-  it('should serialize to CSS', () => {
+
+  it('should serialize a CSS function', () => {
     let rule = call({
       ref: 'rgb',
       args: list([num(100), num(100), num(100)])
     })
     expect(`${rule}`).toBe('rgb(100, 100, 100)')
   })
+
+  it('should serialize a mixin call', () => {
+    let rule = call({
+      ref: 'my-mixin',
+      args: list([num(100), num(100), num(100)])
+    })
+    expect(`${rule}`).toBe('rgb(100, 100, 100)')
+  })
+
   // it('should serialize to a module', () => {
   //   let rule = call({
   //     name: 'rgb',
