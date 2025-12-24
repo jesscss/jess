9115668afe0457a2489a95bacc031a3c4587f2c1
Matthew Dean <matthew-dean@users.noreply.github.com>
2025-12-23 16:59:07 -0800

    Lots of serialization fixes

---
+++ packages/jess/test/less/at-rules.test.ts
@@ -0,0 +1,60 @@
+// @var: 42;
+
+// @media print {
+//     .class {
+//         color: blue;
+//         .sub {
+//             width: @var;
+//         }
+//     }
+//     .top, header > h1 {
+//         color: (#222 * 2);
+//     }
+// }
+
+import { describe, it, expect } from 'vitest';
+import { Compiler } from '../../src';
+
+describe('At Rules', () => {
+  const compiler = new Compiler();
+
+  it('should handle simple at rule', async () => {
+    const lessCode = `
+      @var: 42;
+
+      @media print {
+          .class {
+              color: blue;
+              .sub {
+                  width: @var;
+              }
+          }
+          .top, header > h1 {
+              color: (#222 * 2);
+          }
+      }
+    `;
+    const css = await compiler.renderString(lessCode, {
+      language: 'less',
+      config: {
+        output: {
+          collapseNesting: true
+        }
+      }
+    });
+    expect(css).toBeString(`
+      @media print {
+        .class {
+          color: blue;
+        }
+        .class .sub {
+          width: 42;
+        }
+        .top,
+        header > h1 {
+          color: #444444;
+        }
+      }
+    `);
+  });
+});
