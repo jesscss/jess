6759b6471e43eec8c4f2bb50e3302c7aa055dc7a
Matthew Dean <matthew-dean@users.noreply.github.com>
2026-03-01 11:44:13 -0800

    Advance Less unit fixture sweep through media fixtures.
    This updates the all-less run-through boundary to tests-unit/media/media.less and marks jess-plugin-less-compat as ESM to match current package module semantics.

--- packages/jess/test/less/all-less.test.ts
+++ packages/jess/test/less/all-less.test.ts
@@ -29,7 +29,7 @@ const additionalSkips = [
 ];
 
 // Run unit fixtures alphabetically up through this filename (inclusive).
-const runUnitThrough = 'tests-unit/layer/layer.less';
+const runUnitThrough = 'tests-unit/media/media.less';
 
 describe('Can render Less files to CSS', () => {
   // Get all .less files from tests-unit and tests-config directories
