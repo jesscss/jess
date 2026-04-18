08e24f428b2f7a5bb2b44121afb5a34b9120305c
Matthew Dean <matthew-dean@users.noreply.github.com>
2026-04-18 11:46:08 -0700

    Fix negative syntax serialization

--- packages/core/src/tree/negative.ts
+++ packages/core/src/tree/negative.ts
@@ -2,6 +2,7 @@ import { Node, defineType, F_VISIBLE, F_NON_STATIC } from './node.js';
 import type { Context } from '../context.js';
 import { Dimension } from './dimension.js';
 import { type MaybePromise, pipe, tryStep } from '@jesscss/awaitable-pipe';
+import { getPrintOptions, type PrintOptions } from './util/print.js';
 
 /**
  * The negative sign before a node
@@ -11,10 +12,23 @@ export interface Negative extends Node<Node> {
 }
 
 export class Negative extends Node<Node> {
+  private renderNegativeSyntax(options?: PrintOptions): string {
+    options = getPrintOptions(options);
+    const w = options.writer!;
+    const mark = w.mark();
+    w.add('-');
+    this.value.toString(options);
+    return w.getSince(mark);
+  }
+
   constructor(value: Node, options?: any, location?: any, treeContext?: any) {
     super(value, options, location, treeContext);
     // Negative operations are always non-static, but can inherit may_async from children
     this.addFlags(F_VISIBLE, F_NON_STATIC);
+  }
+
+  override toTrimmedString(options?: PrintOptions): string {
+    return this.renderNegativeSyntax(options);
   }
 
   override evalNode(context: Context): MaybePromise<Node> {
