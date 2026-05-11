ccdcd0ee4da2e6c71f33c79b06921b49e9e3c797
Matthew Dean <matthew-dean@users.noreply.github.com>
2026-05-10 19:04:28 -0700

    test(core): render evaluated arguments binding output

--- packages/core/src/tree/__tests__/mixin.test.ts
+++ packages/core/src/tree/__tests__/mixin.test.ts
@@ -1673,7 +1673,7 @@ describe('Mixin', () => {
         context.root = root;
 
         const evald = await root.eval(context);
-        const css = evald.toString();
+        const css = await evald.render(context);
 
         expect(css).toBeString(`
           .test {
