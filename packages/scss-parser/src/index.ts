176f01cd50126a02b7d429a1826468b466a18dd1
Matthew Dean <matthew-dean@users.noreply.github.com>
2026-07-08 17:01:16 -0700

    fix(scss-parser): avoid duplicate grammar export

--- packages/scss-parser/src/index.ts
+++ packages/scss-parser/src/index.ts
@@ -14,7 +14,6 @@ export type { ScssRules, SyntacticContentAssistSuggestion } from './scssParser.j
 export { ScssParser as ScssParserChevrotain } from './scssParser.js';
 
 export const Parser = ScssParser;
-export { scssGrammar } from './grammar.js';
 export { parseScssCst } from './cst.js';
 export type {
   ScssCstChild, ScssCstError, ScssCstLeaf, ScssCstNode, ScssCstParseResult, ScssCstType
